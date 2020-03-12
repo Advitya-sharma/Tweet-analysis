4d25c347916fe3c33b7d7682d5f8af862e136a7e
Advitya sharma <advityasharma21@gmail.com>
2020-03-13 03:48:09 +0530

    BAR PLOT

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ auth.set_access_token(accessToken,accesssecret)
 api = tweepy.API(auth)
 
 
-tweet = tweepy.Cursor(api.search, q="JNU",tweet_mode="extended",lang = "en",count=200,include_rts=False).items(1000)
+tweet = tweepy.Cursor(api.search, q="#coronavirus",tweet_mode="extended",lang = "en",count=200,include_rts=False).items(100)
 
 df = pd.DataFrame([tweet.full_text for tweet in tweet],columns=['Tweets'])
 
@@ -35,5 +35,34 @@ df['Tweets'] = df['Tweets'].apply(cleantext)
 allwords = ' '.join([tweets for tweets in df['Tweets']])
 
 wordcloud = WordCloud(width=500,height=300,max_font_size=112).generate(allwords)
+
 plt.imshow(wordcloud)
 plt.show()
+
+def getPolarity(text):
+    return TextBlob(text).sentiment.polarity
+
+df['Polarity'] = df["Tweets"].apply(getPolarity)
+
+
+def getAnalysis(score):
+
+    if score < 0:
+        return 'Negative'
+    elif score == 0:
+        return 'Neutral'
+    else:
+        return 'Positive'
+
+df['Analysis'] = df["Polarity"].apply(getAnalysis)
+
+print(df.head())
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+
+ax.hist(df["Analysis"])
+
+plt.show()
+
+print(df["Analysis"].value_counts())
