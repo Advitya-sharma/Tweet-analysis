48b899cd9b795b2e52e1ec10fb8d1eaadca20cbd
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-03-14 05:08:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import seaborn as sns
 
 
 
-consumerKey = "XOxzythWu8hHzvnIapml2XH9O"
-consumerSecret = "LR6DA85OwtFfCYeJpMxJV4SsO5ZIhjvYeZQedgHG4isrccSp6J"
+consumerKey = "gg"
+consumerSecret = "gg"
 
-accessToken = "1192097781667651585-XOcrN0aT1Mqjld3brLJhfdo6dHCeB6"
-accesssecret = "GPgxpjF4YHJVxMjAHX92VfXDICjjK9SbRPlLMhSCk45b5"
+accessToken = "1192097781667651585-"
+accesssecret = "GPgxpjF4YHJVxMjAHXb5"
 
 auth = tweepy.OAuthHandler(consumer_key=consumerKey,consumer_secret=consumerSecret)
 auth.set_access_token(accessToken,accesssecret) 
